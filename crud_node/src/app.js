d1b24b9ef6f7ae9dcb8eb3981c28458892bb6572
GStonDani <gastonsillo_dani_17@hotmail.com>
2019-04-29 12:05:26 -0600

    Apache

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,11 +1,34 @@
-const express = require('express')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mysql = require('mysql'); //sequel
+const myconnection = require('express-myconnection')
 const app = express();
+
 //importar rutas
 const indiceRoutes=require('./rutas/index');
-
+//Configuarciones
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(_dirname,'views'));
 //Usamos rutas
-app.('/').indiceRoutes;
+app.use('/').indiceRoutes;
 
-app.listen(3000, () => {
+//Middleware
+app.use(morgan('dev'));
+app.use(myconnection(mysql,{
+
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port:: 3306,
+	database: 'crudnodejsmysql'
+
+
+},'single'))
+
+//acTIVAR LA CONFUGURACION QUE ESCUCHA
+app.listen(app.get('port'), () => {
 	console.log("Escuchando en puerto 3000");
 })
+
